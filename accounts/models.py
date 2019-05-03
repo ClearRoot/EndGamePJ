f4423827ccc1c657769592dab60152115c8a3530
ClearRoot <jhk7767@gmail.com>
2019-05-03 19:33:00 +0900

    190503 | User model 수정 => profile_image 추가 | JoHogeun

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
 # Create your models here.
 class User(AbstractUser):
+    profile_image= ProcessedImageField(
+        upload_to= 'accounts/images',
+        processors= [ResizeToFill(150,150)],
+        format= 'JPEG',
+        options= {'quality':90},
+        blank=True
+    )
     pass
